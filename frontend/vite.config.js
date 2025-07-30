99acd6bdf6347d34579c68efb5e9a03c16e0c9b0
mann7866 <akunkudewe861@gmail.com>
2025-07-30 16:26:56 +0700

    add

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteStaticCopy({
-    targets: [
-      {
-        src: '_redirects',
-        dest: '.', 
-      },
-    ],
-  }),],
+  plugins: [react(), tailwindcss(),],
 })
