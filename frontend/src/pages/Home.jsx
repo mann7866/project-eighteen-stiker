bd20637a5b75d8e0c78fbb94b7de10e24f3775e5
mann7866 <akunkudewe861@gmail.com>
2025-07-31 15:36:00 +0700

    s

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -135,7 +135,7 @@ export default function HomePage() {
           </p>
         </div>
 
-        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8 py-2 px-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 py-2 p-4">
           {products.map((item, index) => (
             <div
               key={index}
