e6e43267c492d42e80d3be3fce5e7213feae9cc7
mann7866 <akunkudewe861@gmail.com>
2025-09-11 15:33:07 +0700

    a

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -6,9 +6,7 @@ export default function ContactPage() {
     <div className="bg-sky low-sky px-5 sm:px-7 md:px-6 lg:px-15 mx-auto">
       <div className="py-25 md:py-50">
         <h2 className="text-3xl sm:text-4xl font-bold text-center mb-20"
-         data-aos="fade-in"
-              data-aos-duration="1000"
-              data-aos-once="true"
+        
               >
           Kontak Kami
         </h2>
